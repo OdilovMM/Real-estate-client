cf058acf494442146baee1107cb801660209020d
OdilovMM <dssrinman@gmail.com>
2024-05-24 09:57:21 +0500

    Add: Add Page/ components/layout and routes

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+import Filter from "./Filter";
+import Footer from "./Footer";
+import Header from "./Header";
+import Map from "./Map";
+import Pin from "./Pin";
+import Search from "./Search";
+import Slider from "./Slider";
+
+export { Filter, Footer, Header, Map, Pin, Search, Slider };
---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import HomePage from "./HomePage";
+import GalleryPage from "./GalleryPage";
+import LoginPage from "./LoginPage";
+import ProfilePage from "./ProfilePage";
+import RegisterPage from "./RegisterPage";
+import SinglePage from "./SinglePage";
+
+export {
+  HomePage,
+  GalleryPage,
+  LoginPage,
+  ProfilePage,
+  SinglePage,
+  RegisterPage,
+};
